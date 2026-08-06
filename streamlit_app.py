88c0961b8ec36e87300358db9553ce4856a53a95
Ragw54 <53807756+Ragw54@users.noreply.github.com>
2026-08-06 18:42:29 +1000

    Increase font size of selectbox label

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ st.markdown("""
     
     /* Selectbox Label and Larger Selectbox Text */
     .stSelectbox label {
-        font-size: 1.5rem !important;
+        font-size: 1.8rem !important;
         font-weight: 700 !important;
         color: #216bd1 !important;
     }
