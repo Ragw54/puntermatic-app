01f7eb898659311ca2cf11e14ac62175af8e8073
Ragw54 <53807756+Ragw54@users.noreply.github.com>
2026-08-07 06:58:21 +1000

    Increase max-width of grid layout to 600px

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ st.markdown("""
         grid-template-columns: repeat(3, 1fr) !important;
         gap: 8px !important;
         width: 100% !important;
-        max-width: 480px !important;
+        max-width: 600px !important;
         margin: 0 auto !important;
     }
     
