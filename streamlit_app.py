d38f0fa7e001b8994986f2c7096b7cef73c95ee3
Ragw54 <53807756+Ragw54@users.noreply.github.com>
2026-08-07 07:30:45 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ st.markdown("""
         grid-template-columns: repeat(3, 1fr) !important;
         gap: 28px !important;
         width: 100% !important;
-        max-width: 800px !important;
+        max-width: 1200px !important;
         margin: 0 auto !important;
     }
     
